68e360f69c6e1a0ab7254a3e3d6ac5e8bb221db5
Alex <a.n.chasovskoy@gmail.com>
2020-02-17 21:43:46 +0300

    Two options
    Another option added

--- Simple counter.py
+++ Simple counter.py
@@ -1,4 +1,12 @@
-my_list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+my_list = range(1, 11)
+x = 0
+for item in my_list:
+    x = x + item
+print(x)
+
+# or another option
+
+my_list = [1,2,3,4,5,6,7,8,9,10]
 x = 0
 for item in my_list:
     x = x + item
