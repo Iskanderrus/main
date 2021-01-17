45a8de4344592efb0f9f080ea99f4728a58dac2b
Iskanderrus <a.n.chasovskoy@gmail.com>
2021-01-17 23:50:18 +0300

    Fixed the bug for user entering not valid number:  string for example.

--- guess_random_num.py
+++ guess_random_num.py
@@ -2,11 +2,19 @@ import sys
 import random
 
 right_number = random.randint(int(sys.argv[1]), (int(sys.argv[2]) + 1))
-guess = int(input(f'Guess a number in range of {sys.argv[1]} to {sys.argv[2]} --> '))
-while guess != right_number:
-    if guess < int(sys.argv[1]) or guess > int(sys.argv[2]):
-        guess = int(input('Out of range. Try once again: '))
-    else:
-        guess = int(input('Nope... Try once again: '))
-else:
-    print('Well done!')
+while True:
+    try:
+        guess = int(input(f'Guess a number in range of {sys.argv[1]} to {sys.argv[2]} --> '))
+        if guess < int(sys.argv[1]) or guess > int(sys.argv[2]):
+            print('Out of range. Try once again.')
+            continue
+        else:
+            if right_number == guess:
+                print('Well done!')
+                break
+            else:
+                print('Nope... Try once again.')
+                continue
+    except ValueError:
+        print('Please enter a valid number.')
+        continue
