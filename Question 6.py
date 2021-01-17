c66db1ebf316c6650826b2d75a961aaed4d14436
Iskanderrus <a.n.chasovskoy@gmail.com>
2021-01-18 00:08:41 +0300

    Question 6 from https://github.com/darkprinx/100-plus-Python-programming-exercises-extended/blob/master/Status/Day%202.md

--- Question 6.py
+++ Question 6.py
@@ -2,10 +2,9 @@ import math
 
 c, h = 50, 30
 
-my_list = input('Enter numbers separated by a comma ')
-numbers = my_list.split(',')
+my_list = input('Enter numbers separated by a comma ').split(',')
 squares = []
 
-for d in numbers:
+for d in my_list:
     squares.append(str(int(round(math.sqrt(2*c*float(d)/h)))))
 print(','.join(squares))
