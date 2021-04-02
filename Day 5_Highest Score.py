4f5c0dc120402a7b523b2b9a7f1f13e902d8cb51
Iskanderrus <alex_ru2002@list.ru>
2021-04-02 23:43:36 +0300

    Day 5 exercises

--- Day 5_Highest Score.py
+++ Day 5_Highest Score.py
@@ -4,4 +4,4 @@ for n in range(0, len(student_scores)):
     student_scores[n] = int(student_scores[n])
     if student_scores[n] > highest:
         highest = student_scores[n]
-print(highest)
+print(f'The highest score in the class is: {highest}.')
