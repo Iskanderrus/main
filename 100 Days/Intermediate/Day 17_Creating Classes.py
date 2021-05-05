090464a6c0c10a51247442a03ecde0df3c7f5481
Iskanderrus <alex_ru2002@list.ru>
2021-05-06 00:00:16 +0300

    Days 16 and 17 - OOP basics and Class creation

--- 100 Days/Intermediate/Day 17_Creating Classes.py
+++ 100 Days/Intermediate/Day 17_Creating Classes.py
@@ -1,8 +1,9 @@
 class RaR:
 
-    def __init__(self, name, family_name, partner, location,
+    def __init__(self, rar_id, name, family_name, partner, location,
                  field_sales, supervisor, net_salary, net_transport_costs,
-                 type, status):
+                 rar_type, status):
+        self.id = rar_id
         self.name = name
         self.family_name = family_name
         self.partner = partner
@@ -11,19 +12,71 @@ class RaR:
         self.location = location
         self.net_salary = net_salary
         self.net_transport_costs = net_transport_costs
-        self.type = type
+        self.rar_type = rar_type
         self.status = status
+        self.target_m1q1 = 0
+        self.target_m1q2 = 0
+        self.target_m1q3 = 0
+        self.target_m1q4 = 0
+        self.target_m2q1 = 0
+        self.target_m2q2 = 0
+        self.target_m2q3 = 0
+        self.target_m2q4 = 0
+        self.target_m3q1 = 0
+        self.target_m3q2 = 0
+        self.target_m3q3 = 0
+        self.target_m3q4 = 0
+        self.target_m4q1 = 0
+        self.target_m4q2 = 0
+        self.target_m4q3 = 0
+        self.target_m4q4 = 0
+        self.fact_m1q1 = 0
+        self.fact_m1q2 = 0
+        self.fact_m1q3 = 0
+        self.fact_m1q4 = 0
+        self.fact_m2q1 = 0
+        self.fact_m2q2 = 0
+        self.fact_m2q3 = 0
+        self.fact_m2q4 = 0
+        self.fact_m3q1 = 0
+        self.fact_m3q2 = 0
+        self.fact_m3q3 = 0
+        self.fact_m3q4 = 0
+        self.fact_m4q1 = 0
+        self.fact_m4q2 = 0
+        self.fact_m4q3 = 0
+        self.fact_m4q4 = 0
+        self.transport_q1 = 0
+        self.transport_q2 = 0
+        self.transport_q3 = 0
+        self.transport_q4 = 0
 
     def bds_type_change(self, commodity):
-        self.type = f"BDS {commodity}"
+        self.rar_type = f"BDS {commodity}"
 
     def field_type_change(self, location):
-        self.type = f"Regional - {location}"
+        self.rar_type = f"Regional - {location}"
+
+    def quarterly_salary_calculation(self, rar_id):
+        pass
+
+    def salary_increase(self, rar_id, amount):
+        pass
+
+    def transport_costs_review(self, rar_id, amount):
+        pass
+
+    def rar_efficiency(self, rar_id):
+        pass
+
+    def targets_calculation(self, rar_id):
+        pass
 
 
-rar_1 = RaR("Ivan", "Demidov", "Partner1", "Rostov-on-Don", "Petr Zhmykha", "Atanas Zubka", 40000, 5000, "field",
+rar_1 = RaR("Ivan", "Demidov", "Partner1", "Rostov-on-Don", "Petr Zhmykha",
+            "Atanas Zubka", 40000, 5000, "field",
             "active")
 rar_1.bds_type_change("Welding")
 rar_1.field_type_change("Voronezh Oblast")
 
-print(rar_1.type)
+print(rar_1.rar_type)
