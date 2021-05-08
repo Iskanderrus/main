48994ea182c6ee622bdcca2d142ae33c9d1a97be
Iskanderrus <alex_ru2002@list.ru>
2021-05-08 23:38:49 +0300

    Days 17 - OOP Question and Answer Game

--- 100 Days/Intermediate/Day 17 - OOP Solution/main.py
+++ 100 Days/Intermediate/Day 17 - OOP Solution/main.py
@@ -1,4 +1,10 @@
+from question_model import Question
+from data import question_data
 
 
-
-
+data_bank = []
+for question in question_data:
+    question_text = question["text"]
+    question_answer = question["answer"]
+    new_question = Question(question_text, question_answer)
+    data_bank.append(new_question)
--- 100 Days/Intermediate/Day 17 - OOP Solution/question_model.py
+++ 100 Days/Intermediate/Day 17 - OOP Solution/question_model.py
@@ -8,14 +8,3 @@ class Question:
         self.answer = answer
 
 
-data_bank = []
-counter = 0
-while counter < len(data.question_data):
-    for question in data.question_data:
-        new_text = data.question_data[counter]["text"]
-        new_answer = data.question_data[counter]["answer"]
-        new_question = Question(new_text, new_answer)
-        data_bank.append(new_question)
-        counter += 1
-
-print(data_bank)
