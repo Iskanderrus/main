0870f1020427db303833bd75724e87c2b9fa34a4
Iskanderrus <alex_ru2002@list.ru>
2021-05-09 00:11:46 +0300

    Days 17 - OOP Question and Answer Game

--- 100 Days/Intermediate/Day 17 - OOP Solution/quiz_brain.py
+++ 100 Days/Intermediate/Day 17 - OOP Solution/quiz_brain.py
@@ -15,7 +15,7 @@ class QuizBrain:
         self.check_answer(user_answer, current_question.answer)
 
     def check_answer(self, user_answer, correct_answer):
-        if user_answer == correct_answer:
+        if user_answer.lower() == correct_answer.lower():
             self.score += 1
             print("This is correct answer.")
         else:
