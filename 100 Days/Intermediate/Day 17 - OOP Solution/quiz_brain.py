81de0d4d03ee2b0e7313e5ee2f1bc7589b1da58c
Iskanderrus <alex_ru2002@list.ru>
2021-05-09 00:09:47 +0300

    Days 17 - OOP Question and Answer Game

--- 100 Days/Intermediate/Day 17 - OOP Solution/quiz_brain.py
+++ 100 Days/Intermediate/Day 17 - OOP Solution/quiz_brain.py
@@ -3,6 +3,7 @@ class QuizBrain:
     def __init__(self, q_list):
         self.question_number = 0
         self.question_list = q_list
+        self.score = 0
 
     def still_has_questions(self):
         return self.question_number < len(self.question_list)
@@ -10,4 +11,14 @@ class QuizBrain:
     def next_question(self):
         current_question = self.question_list[self.question_number]
         self.question_number += 1
-        input(f"Q.{self.question_number}: {current_question.text} (True/False): ")
+        user_answer = input(f"Q.{self.question_number}: {current_question.text} (True/False): ")
+        self.check_answer(user_answer, current_question.answer)
+
+    def check_answer(self, user_answer, correct_answer):
+        if user_answer == correct_answer:
+            self.score += 1
+            print("This is correct answer.")
+        else:
+            self.score += 0
+            print("This is wrong answer.")
+        print(f"Your score is {self.score} of {len(self.question_list)}. \n")
