d014d662ce3c745e75821afe17c7f47f3db7e876
Iskanderrus <alex_ru2002@list.ru>
2021-05-10 20:42:04 +0300

    Day 18 - Random walk function, incl. random walk, random color and growing thickness of the drawing line

---
+++ 100 Days/Intermediate/Day 18 - GUI/Random_Walk.py
@@ -0,0 +1,22 @@
+from turtle import Turtle, Screen
+import random
+
+tim = Turtle()
+tim.shape("turtle")
+tim.speed("fastest")
+
+def random_walk(distance, angle, turtle_color):
+    tim.forward(distance)
+    tim.right(angle)
+    tim.color(turtle_color)
+
+
+angles = [0, 90, 180, 270]
+
+colors = ["red", "blue", "orange", "black", "green", "gray", "gold", "lime", "deep sky blue", "magenta"]
+for x in range(200):
+    tim.width(x * 0.05)
+    random_walk(30, random.choice(angles), random.choice(colors))
+
+screen = Screen()
+screen.exitonclick()
