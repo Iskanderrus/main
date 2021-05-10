716acf171079d8907a170674b634f740677b36a1
Iskanderrus <alex_ru2002@list.ru>
2021-05-10 23:02:43 +0300

    Day 18 - Random walk - true random color and random angle added

--- 100 Days/Intermediate/Day 18 - GUI/Random_Walk.py
+++ 100 Days/Intermediate/Day 18 - GUI/Random_Walk.py
@@ -1,23 +1,31 @@
-from turtle import Turtle, Screen
+from turtle import Turtle, Screen, colormode
 import random
 
 tim = Turtle()
 tim.shape("turtle")
 tim.speed("fast")
+colormode(255)
 
 
-def random_walk(distance, angle, turtle_color):
+def random_walk(distance, angle):
     tim.forward(distance)
     tim.right(angle)
-    tim.color(turtle_color)
 
 
-angles = [0, 90, 180, 270]
+def random_color():
+    red = random.randint(0, 255)
+    green = random.randint(0, 255)
+    blue = random.randint(0, 255)
+    color = (red, green, blue)
+    return color
 
-colors = ["red", "blue", "orange", "black", "green", "gray", "gold", "lime", "deep sky blue", "magenta"]
+
 for x in range(200):
-    tim.width(x * 0.05)
-    random_walk(30, random.choice(angles), random.choice(colors))
+    angles = random.randint(0, 360)
+    tim.pensize(x * 0.05)
+    tim.pencolor(random_color())
+    random_walk(30, angles)
+
 
 screen = Screen()
 screen.exitonclick()
