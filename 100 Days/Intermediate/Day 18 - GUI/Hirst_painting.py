59dbdc8998e095195a7382f2316a0e433bb9032d
Iskanderrus <alex_ru2002@list.ru>
2021-05-12 23:29:50 +0300

    Day 18 - Final project of the day (Painting a circled picture) - colors imported from real picture using colorgram

--- 100 Days/Intermediate/Day 18 - GUI/Hirst_painting.py
+++ 100 Days/Intermediate/Day 18 - GUI/Hirst_painting.py
@@ -7,7 +7,7 @@ tim.shape("turtle")
 colormode(255)
 tim.speed("fastest")
 imported_colors = colorgram.extract('images/image.jpg', 20)
-
+tim.hideturtle()
 
 class Rgb:
 
@@ -28,31 +28,27 @@ for color in imported_colors:
     colors_list.append(color)
 
 
-def hirst_painter(picture_size, radius, offset, colors):
+def hirst_painter(picture_size, dot_size, offset, colors):
     """
     Function to draw a masterpiece of circles having the same diameter and offset but different colors.
     :param picture_size: Number of circles in the line and in column.
-    :param radius: Defines size of the circle.
+    :param dot_size: Defines size of the circle.
     :param offset: Distance between the circles.
     :param colors: Colors to be used in painting
     :return: None
     """
-    y = -480
+    y = -450
     tim.penup()
     tim.setposition(-480, y)
     for _ in range(picture_size):
         for _ in range(picture_size):
-            tim.fillcolor(random.choice(colors))
-            tim.begin_fill()
-            tim.circle(radius)
-            tim.end_fill()
-            tim.penup()
-            tim.forward(radius * 2 + offset)
-        y += (radius * 2 + offset)
+            tim.dot(dot_size, random.choice(colors))
+            tim.forward(dot_size + offset)
+        y += (dot_size + offset)
         tim.setposition(-480, y)
     tim.hideturtle()
 
 
-hirst_painter(picture_size=12, radius=40, offset=17, colors=colors_list)
+hirst_painter(picture_size=10, dot_size=40, offset=17, colors=colors_list)
 screen = Screen()
 screen.exitonclick()
