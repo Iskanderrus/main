95a46cf9049b5d7decd965cf11de28b895f65134
Iskanderrus <alex_ru2002@list.ru>
2021-05-14 23:59:27 +0300

    Day 19 - Turtle race

--- 100 Days/Intermediate/Day 19 - Event Listeners/Several turtles.py
+++ 100 Days/Intermediate/Day 19 - Event Listeners/Several turtles.py
@@ -1,20 +1,36 @@
 import random
+import turtle
 from turtle import Turtle, Screen
 
+is_race_on = False
 screen = Screen()
 screen.setup(width=500, height=400)
 user_bet = screen.textinput(title="Make your bet", prompt="Which turtle will win the race? Enter a color: ")
-colors = ["red", "green", "blue", "black", "purple", "orange"]
+colors = ["red", "green", "blue", "grey", "purple", "orange"]
 y_coordinate = [-70, -40, -10, 20, 50, 80]
+all_turtles = []
 
 for turtle_number in range(len(colors)):
-    tim = Turtle(shape="turtle")
-    tim.penup()
-    tim.color(colors[turtle_number])
-    tim.setposition(x=-230, y=y_coordinate[turtle_number])
+    new_turtle = Turtle(shape="turtle")
+    new_turtle.penup()
+    new_turtle.color(colors[turtle_number])
+    new_turtle.setposition(x=-230, y=y_coordinate[turtle_number])
+    all_turtles.append(new_turtle)
 
+while user_bet not in colors:
+    user_bet = screen.textinput(title="Make your bet", prompt="Wrong input. Enter a color: ")
+else:
+    is_race_on = True
 
-while tim.xcor() < 500:
-    tim.forward(random.randint(5, 15))
-
+while is_race_on:
+    for turtle in all_turtles:
+        if turtle.xcor() > 230:
+            winning_color = turtle.pencolor()
+            is_race_on = False
+        random_distance = random.randint(0, 10)
+        turtle.forward(random_distance)
+if user_bet == winning_color:
+    print(f"You win. {winning_color.title()} turtle won the race.")
+else:
+    print(f"You lose. {winning_color.title()} turtle was the first in this race.")
 screen.exitonclick()
