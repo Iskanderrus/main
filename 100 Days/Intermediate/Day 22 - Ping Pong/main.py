039df7a5b38124629baa546996d3f0ed19a0ed1c
Iskanderrus <alex_ru2002@list.ru>
2021-05-30 23:30:17 +0300

    Day 22 - Ping-Pong game - Speed increase added.

--- 100 Days/Intermediate/Day 22 - Ping Pong/main.py
+++ 100 Days/Intermediate/Day 22 - Ping Pong/main.py
@@ -27,8 +27,9 @@ screen.onkeypress(right_paddle.down, "Down")
 # Main game cycle:
 game_is_on = True
 
+
 while game_is_on:
-    time.sleep(0.1)
+    time.sleep(ball.move_speed)
     screen.update()
     ball.move()
 
