38555b6c1c3b2b085cb919a2c90f54ee904e7b24
Iskanderrus <alex_ru2002@list.ru>
2021-05-30 18:06:41 +0300

    Day 22 - Ping-Pong game - Ball class created. Collision with walls and paddles added.

--- 100 Days/Intermediate/Day 22 - Ping Pong/main.py
+++ 100 Days/Intermediate/Day 22 - Ping Pong/main.py
@@ -1,5 +1,6 @@
 from turtle import Screen
 from paddle import Paddle
+from ball import Ball
 import time
 
 # Screen settings:
@@ -12,6 +13,7 @@ screen.tracer(0)
 # Introducing main parts of the game:
 left_paddle = Paddle((-380, 0))
 right_paddle = Paddle((380, 0))
+ball = Ball()
 
 # Introducing controls:
 screen.listen()
@@ -24,24 +26,20 @@ screen.onkeypress(right_paddle.down, "Down")
 game_is_on = True
 
 while game_is_on:
+    time.sleep(0.1)
     screen.update()
-    time.sleep(0.00001)
-#
-#     # Detect collision with the food:
-#     if snake.head.distance(food) < 15:
-#         food.refresh()
-#         snake.extend_snake()
-#         scoreboard.board_refresh()
-#
-#     # Detect collision with the wall:
-#     if snake.head.xcor() > 280 or snake.head.xcor() < -280 or snake.head.ycor() > 280 or snake.head.ycor() < -280:
-#         game_is_on = False
-#         scoreboard.game_over()
-#
-#     # Detect collision with own tail:
-#     for segment in snake.segments[1:]:
-#         if snake.head.distance(segment) < 10:
-#             game_is_on = False
-#             scoreboard.game_over()
+    ball.move()
+
+    # Detect collision with the upper or lower wall:
+    if ball.ycor() > 280 or ball.ycor() < -280:
+        ball.bounce()
+
+    # Detect collision with the right paddle:
+    if ball.distance(right_paddle) < 65 and ball.xcor() == 370:
+        ball.bounce_back()
+
+    # Detect collision with the right paddle:
+    if ball.distance(left_paddle) < 65 and ball.xcor() == -370:
+        ball.bounce_back()
 
 screen.exitonclick()
