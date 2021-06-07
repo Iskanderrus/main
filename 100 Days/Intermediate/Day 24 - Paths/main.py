52b76c6388d1eb3f492ee4159b1006532b5e083e
Iskanderrus <alex_ru2002@list.ru>
2021-06-07 23:37:25 +0300

    Day 24 - High Score added to the Snake game

---
+++ 100 Days/Intermediate/Day 24 - Paths/main.py
@@ -0,0 +1,52 @@
+from turtle import Screen
+from snake import Snake
+from food import Food
+from scoreboard import Scoreboard
+import time
+
+# Screen settings:
+screen = Screen()
+screen.setup(width=600, height=600)
+screen.bgcolor("black")
+screen.title("Голодный червячок")
+screen.tracer(0)
+
+# Introducing main parts of the game:
+snake = Snake()
+food = Food()
+scoreboard = Scoreboard()
+scoreboard.board_refresh()
+
+# Introducing controls:
+screen.listen()
+screen.onkey(snake.up, "Up")
+screen.onkey(snake.down, "Down")
+screen.onkey(snake.left, "Left")
+screen.onkey(snake.right, "Right")
+
+# Main game cycle:
+game_is_on = True
+
+while game_is_on:
+    screen.update()
+    time.sleep(0.2)
+    snake.move()
+
+    # Detect collision with the food:
+    if snake.head.distance(food) < 15:
+        food.refresh()
+        snake.extend_snake()
+        scoreboard.board_refresh()
+
+    # Detect collision with the wall:
+    if snake.head.xcor() > 280 or snake.head.xcor() < -280 or snake.head.ycor() > 280 or snake.head.ycor() < -280:
+        snake.head.goto(0, 0)
+        scoreboard.reset()
+
+    # Detect collision with own tail:
+    for segment in snake.segments[1:]:
+        if snake.head.distance(segment) < 10:
+            game_is_on = False
+            scoreboard.reset()
+
+screen.exitonclick()
---
+++ 100 Days/Intermediate/Day 24 - Paths/scoreboard.py
@@ -0,0 +1,31 @@
+from turtle import Turtle
+
+ALIGNMENT = "center"
+FONT = ("Courier", 15, "bold")
+
+
+class Scoreboard(Turtle):
+
+    def __init__(self):
+        super().__init__()
+        self.counter = -1
+        self.penup()
+        self.high_score = 0
+        self.color('white')
+        self.hideturtle()
+        self.goto(0, 260)
+
+    def reset(self):
+        if self.counter > self.high_score:
+            self.high_score = self.counter
+        self.counter = 0
+        self.board_refresh()
+
+    def board_refresh(self):
+        """
+        Function to clean the scoreboard of the game and assign the new score.
+        :return: None
+        """
+        self.clear()
+        self.counter += 1
+        self.write(f"Счёт: {self.counter}. Наивысший балл: {self.high_score}.", align=ALIGNMENT, font=FONT)
