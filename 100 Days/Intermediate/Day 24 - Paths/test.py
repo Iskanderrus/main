eeb582ac597f24d859225b4854eeff005676a1bb
Iskanderrus <alex_ru2002@list.ru>
2021-06-08 20:34:40 +0300

    Day 24 - Linux Path to the file - both Absolute and Relative Path

--- 100 Days/Intermediate/Day 24 - Paths/test.py
+++ 100 Days/Intermediate/Day 24 - Paths/test.py
@@ -1,8 +1,20 @@
+# Absolute path
 with open("/home/alex/Рабочий стол/my_file.txt") as text:
     contents = text.read()
     print(contents)
 
 path = "/home/alex/Рабочий стол/"
+file = "my_file.txt"
+with open(path + file) as text:
+    contents = text.read()
+    print(contents)
+
+# Relative path
+with open("../../../../../Рабочий стол/my_file.txt") as text:
+    contents = text.read()
+    print(contents)
+
+path = "../../../../../Рабочий стол/"
 file = "my_file.txt"
 with open(path + file) as text:
     contents = text.read()
