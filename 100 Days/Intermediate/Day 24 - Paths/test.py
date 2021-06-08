1e7c1a1c4a4392f5ec49240401b0951c76af4a5a
Iskanderrus <alex_ru2002@list.ru>
2021-06-08 20:26:30 +0300

    Day 24 - Linux Path to the file - Version II

--- 100 Days/Intermediate/Day 24 - Paths/test.py
+++ 100 Days/Intermediate/Day 24 - Paths/test.py
@@ -1,3 +1,9 @@
 with open("/home/alex/Рабочий стол/my_file.txt") as text:
     contents = text.read()
     print(contents)
+
+path = "/home/alex/Рабочий стол/"
+file = "my_file.txt"
+with open(path + file) as text:
+    contents = text.read()
+    print(contents)
