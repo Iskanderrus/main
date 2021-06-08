bf1a96c673e5a2a4b316af7ba6cb405cb4f2d11a
Iskanderrus <alex_ru2002@list.ru>
2021-06-08 23:08:12 +0300

    Day 24 - Birthday invitator added - takes list of the names from one directory, letter template from another and saves a new letter into the ReadyToSend directory.

--- 100 Days/Intermediate/Day 24 - Paths/invitator.py
+++ 100 Days/Intermediate/Day 24 - Paths/invitator.py
@@ -1,14 +1,12 @@
-with open("People/mailing_list.txt") as file:
+with open("Input/People/mailing_list.txt") as file:
     names = file.readlines()
-    new_names = []
-    for name in range(0, 7):
-        new_names.append(names[name][:-1])
-    new_names.append(names[-1])
-    for person in new_names:
-        with open("Letter_templates/birthday_invitation.txt", "r") as text:
+    stripped_names = []
+    for name in names:
+        stripped_names.append(name.strip())
+    for person in stripped_names:
+        with open("Input/Letter_templates/birthday_invitation.txt", "r") as text:
             contents = text.read()
             contents = contents.replace("[name]", person)
 
-        new_letter = open(f"Output/ReadyToSend/ invitation_for_{person}.txt", mode="w")
-        new_letter.write(contents)
-        new_letter.close()
+        with open(f"Output/ReadyToSend/ invitation_for_{person}.txt", mode="w") as new_letter:
+            new_letter.write(contents)
