b532e22f381534d7053d7ca375f4c72f6e4a8645
Iskanderrus <alex_ru2002@list.ru>
2021-06-12 23:03:54 +0300

    Day 26 - Intro to List comprehension

---
+++ 100 Days/Intermediate/Day 26 - List and Dict Comprehencion/main.py
@@ -0,0 +1,11 @@
+"""
+Basic rule:
+            new_list = [new_item for item in old_list]
+"""
+
+# Sample:
+sides_of_squares = [1, 2, 3, 4, 5]
+squares_areas = [item * item for item in sides_of_squares]
+print(squares_areas)
+
+
