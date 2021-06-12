7a2b8c565194c1cfc36e0996618d0e7433b2fd69
Iskanderrus <alex_ru2002@list.ru>
2021-06-12 23:32:26 +0300

    Day 26 - Exercise 2

--- 100 Days/Intermediate/Day 26 - List and Dict Comprehencion/main.py
+++ 100 Days/Intermediate/Day 26 - List and Dict Comprehencion/main.py
@@ -33,9 +33,12 @@ print(short_names)
 print(long_names)
 
 # Exercises
-
+# 1
 numbers = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55]
 squares_numbers = [number * number for number in numbers]
 print(squares_numbers)
 
-
+# 2
+numbers = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55]
+even_numbers = [number for number in numbers if (number % 2 == 0)]
+print(even_numbers)
