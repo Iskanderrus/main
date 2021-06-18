69ed062a896daf982fcc194f75955bc7f02080ec
Iskanderrus <alex_ru2002@list.ru>
2021-06-19 00:08:45 +0300

    Day 27 - Tkinter - Grid and Padding

--- 100 Days/Intermediate/Day 27 - Tkinter - GUI/main.py
+++ 100 Days/Intermediate/Day 27 - Tkinter - GUI/main.py
@@ -1,32 +1,30 @@
 import tkinter as tk
 
 
-window = tk.Tk()
-window.title("My first GUI Program")
-window.minsize(width=500, height=300)
-
-# Label
-my_label = tk.Label(text="Хочешь попасть в Америку?", font=("Arial", 24, "italic")) # --> creating a component
-my_label.pack() # --> run the component
-
-
-# Button
-
 def button_clicked():
     my_label.config(text="Поступай служить\nв ракетные войска!")
 
 
+window = tk.Tk()
+window.title("My first GUI Program")
+window.minsize(width=800, height=300)
+window.config(padx=20, pady=20)
+
+# Label
+my_label = tk.Label(text="Хочешь попасть в Америку?", font=("Arial", 24, "italic"))  # --> creating a component
+my_label.grid(row=0, column=0)  # --> run the component
+my_label.config(padx=30, pady=30)
+
+# Button
 button = tk.Button(text="ДА!", command=button_clicked)
-button.pack()
+button.grid(row=1, column=1)
+
+button2 = tk.Button(text="No!!!")
+button2.grid(row=0, column=2)
 
 # Entry
 my_input = tk.Entry()
 my_input.insert(1, string="Номер телефона")
-my_input.pack()
-
-
-
-
-
+my_input.grid(row=3, column=3)
 
 window.mainloop()
