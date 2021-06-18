1bd5e0a26364fb85d3d1ade7911b12d187975d08
Iskanderrus <alex_ru2002@list.ru>
2021-06-18 23:50:02 +0300

    Day 27 - Tkinter - Basic components samples

--- 100 Days/Intermediate/Day 27 - Tkinter - GUI/main.py
+++ 100 Days/Intermediate/Day 27 - Tkinter - GUI/main.py
@@ -4,9 +4,6 @@ import tkinter as tk
 window = tk.Tk()
 window.title("My first GUI Program")
 window.minsize(width=500, height=300)
-background_image = tk.PhotoImage("../Day 25 - CSV Files/US States Game/blank_states_img.gif")
-background_label = tk.Label(image=background_image)
-background_label.place(x=0, y=0, relwidth=1, relheight=1)
 
 # Label
 my_label = tk.Label(text="Хочешь попасть в Америку?", font=("Arial", 24, "italic")) # --> creating a component
@@ -15,13 +12,19 @@ my_label.pack() # --> run the component
 
 # Button
 
-
 def button_clicked():
-    my_label.config(text="Поступай служить \nв ракетные войска!")
+    my_label.config(text="Поступай служить\nв ракетные войска!")
 
 
 button = tk.Button(text="ДА!", command=button_clicked)
 button.pack()
+
+# Entry
+my_input = tk.Entry()
+my_input.insert(1, string="Номер телефона")
+my_input.pack()
+
+
 
 
 
