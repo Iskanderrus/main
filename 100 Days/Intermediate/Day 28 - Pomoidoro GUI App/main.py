3e35b28b654e19701d7d2f35c8343eb58ef133a8
Iskanderrus <alex_ru2002@list.ru>
2021-06-19 22:48:34 +0300

    Day 28 - GUI of Pomodoro App: labels and buttons added

--- 100 Days/Intermediate/Day 28 - Pomoidoro GUI App/main.py
+++ 100 Days/Intermediate/Day 28 - Pomoidoro GUI App/main.py
@@ -27,6 +27,18 @@ canvas = Canvas(width=203, height=224, bg=YELLOW, highlightthickness=0)
 background_image = PhotoImage(file="tomato.png")
 canvas.create_image(103, 112, image=background_image)
 canvas.create_text(110, 143, text="00:00", fill="white", font=(FONT_NAME, 25, "bold"))
-canvas.pack()
+canvas.grid(column=1, row=1)
+
+timer_label = Label(text="Timer", font=("Chilanka", 30, "bold"), bg=YELLOW, fg=GREEN)
+timer_label.grid(column=1, row=0)
+
+checkmark_label1 = Label(text="✓", font=(FONT_NAME, 30, "bold"), bg=YELLOW, fg=GREEN)
+checkmark_label1.grid(column=1, row=3)
+
+start_button = Button(text="Start", bg=YELLOW, highlightthickness=0)
+start_button.grid(column=0, row=2)
+
+reset_button = Button(text="Reset", bg=YELLOW, highlightthickness=0)
+reset_button.grid(column=2, row=2)
 
 window.mainloop()
