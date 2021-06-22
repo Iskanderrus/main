600dc2ca68132c79c7ad1de8139c488d932a8772
Iskanderrus <alex_ru2002@list.ru>
2021-06-22 23:17:42 +0300

    Day 30 - Exercise 3 - Solution 2

--- 100 Days/Intermediate/Day 26 - List and Dict Comprehencion/NATO Alphabet.py
+++ 100 Days/Intermediate/Day 26 - List and Dict Comprehencion/NATO Alphabet.py
@@ -1,15 +1,32 @@
 import pandas as pd
 
+# df = pd.read_csv("nato_phonetic_alphabet.csv")
+#
+# done = False
+# while not done:
+#     word = input("Enter the word:\n").upper()
+#     output_dict = {row.letter: row.code for (index, row) in df.iterrows()}
+#     try:
+#         list_of_codes = [output_dict[letter] for letter in word]
+#     except KeyError:
+#         print("Sorry, only letters in Latin alphabet, please.")
+#     else:
+#         done = True
+#         print(list_of_codes)
+
 df = pd.read_csv("nato_phonetic_alphabet.csv")
 
-done = False
-while not done:
+
+def phonetic_spell():
     word = input("Enter the word:\n").upper()
     output_dict = {row.letter: row.code for (index, row) in df.iterrows()}
     try:
         list_of_codes = [output_dict[letter] for letter in word]
     except KeyError:
         print("Sorry, only letters in Latin alphabet, please.")
+        phonetic_spell()
     else:
-        done = True
         print(list_of_codes)
+
+
+phonetic_spell()
