a77f622b3bc9f26c59cd3825141a4596c2acf141
Iskanderrus <alex_ru2002@list.ru>
2021-06-22 22:18:42 +0300

    Day 30 - Exceptions catching - Standard Errors

--- 100 Days/Intermediate/Day 30 - JSON files/try-except.py
+++ 100 Days/Intermediate/Day 30 - JSON files/try-except.py
@@ -4,14 +4,14 @@ try:
     file = open("a_file.txt")
     a_dictionary = {"key": "value"}
     print(a_dictionary["key"])
-except FileNotFoundError:
+except FileNotFoundError:  # just except is too wide - needs to be specified for each error to catch and handle it
     file = open("a_file.txt", "a")
     file.write("Some message will be printed here")
 except KeyError as error_message:
     print(f"There is no key {error_message} in the a_dictionary")
-else: # --> if all is ok without any error
+else:  # --> if all is ok without any error
     content = file.read()
     print(content)
-finally: # code to be run no matter what
+finally:  # code to be run no matter what
     file.close()
     print("File was closed")
