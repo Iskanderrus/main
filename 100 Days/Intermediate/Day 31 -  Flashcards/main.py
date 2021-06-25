11ecb102471e313676ef88b68424283678451bcb
Iskanderrus <alex_ru2002@list.ru>
2021-06-25 23:36:49 +0300

    Day 31 - Flashcard for language learning - GUI only

--- 100 Days/Intermediate/Day 31 -  Flashcards/main.py
+++ 100 Days/Intermediate/Day 31 -  Flashcards/main.py
@@ -3,7 +3,7 @@ from tkinter import *
 BACKGROUND_COLOR = "#B1DDC6"
 
 window = Tk()
-window.title("Mameloshn")
+window.title("מאַמע־לשון‎")
 window.config(padx=50, pady=50, bg=BACKGROUND_COLOR)
 
 canvas = Canvas(width=800, height=526)
@@ -14,12 +14,12 @@ canvas.create_text(400, 263, text="WORD", font=("Arial", 60, "bold"))
 canvas.config(bg=BACKGROUND_COLOR, highlightthickness=0)
 canvas.grid(row=0, column=0, columnspan=2)
 
-cross_image = PhotoImage("images/wrong.png", width=100, height=99)
-unknown_button = Button(image=cross_image, highlightthickness=0)
+cross_image = PhotoImage(file="images/wrong.png", width=100, height=99)
+unknown_button = Button(image=cross_image, highlightthickness=0, borderwidth=0)
 unknown_button.grid(row=1, column=0)
 
-check_image = PhotoImage("images/right.png", width=100, height=99)
-unknown_button = Button(image=check_image, highlightthickness=0)
+check_image = PhotoImage(file="images/right.png", width=100, height=99)
+unknown_button = Button(image=check_image, highlightthickness=0, borderwidth=0)
 unknown_button.grid(row=1, column=1)
 
 
