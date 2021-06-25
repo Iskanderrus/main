183f86acdd79beb7ac56fafeea9037c7a55cde0a
Iskanderrus <alex_ru2002@list.ru>
2021-06-26 00:29:10 +0300

    Day 31 - Flashcard for language learning - Flipping is added

--- 100 Days/Intermediate/Day 31 -  Flashcards/main.py
+++ 100 Days/Intermediate/Day 31 -  Flashcards/main.py
@@ -2,30 +2,41 @@ from tkinter import *
 import pandas
 import random
 
-
 BACKGROUND_COLOR = "#B1DDC6"
 
 data = pandas.read_csv("data.csv")
 to_learn = data.to_dict(orient="records")
+current_card = dict()
 
 
 def next_card():
+    global current_card, flip_timer
+    window.after_cancel(flip_timer)
     current_card = random.choice(to_learn)
-    canvas.itemconfig(card_title, text="Armenian Letter")
-    canvas.itemconfig(card_word, text=current_card["Armenian Letter"])
+    canvas.itemconfig(card_title, text="Armenian Letter", fill="black")
+    canvas.itemconfig(card_word, text=current_card["Armenian Letter"], fill="black")
+    canvas.itemconfig(card_background, image=card_front_img)
+    flip_timer = window.after(3000, func=flip_card)
 
 
+def flip_card():
+    canvas.itemconfig(card_title, text="Meaning", fill="white")
+    canvas.itemconfig(card_word, text=current_card["Meaning"], fill="white")
+    canvas.itemconfig(card_background, image=card_back_img)
 
 
 window = Tk()
 window.title("հայերեն")
 window.config(padx=50, pady=50, bg=BACKGROUND_COLOR)
 
+flip_timer = window.after(3000, func=flip_card)
+
 canvas = Canvas(width=800, height=526)
 card_front_img = PhotoImage(file="images/card_front.png")
-canvas.create_image(400, 263, image=card_front_img)
-card_title = canvas.create_text(400, 158, text="Title", font=("Arial", 40, "italic"))
-card_word = canvas.create_text(400, 263, text="WORD", font=("Arial", 60, "bold"))
+card_back_img = PhotoImage(file="images/card_back.png")
+card_background = canvas.create_image(400, 263, image=card_front_img)
+card_title = canvas.create_text(400, 158, text="Title", font=("Arial", 30, "italic"))
+card_word = canvas.create_text(400, 263, text="WORD", font=("Arial", 25, "bold"))
 canvas.config(bg=BACKGROUND_COLOR, highlightthickness=0)
 canvas.grid(row=0, column=0, columnspan=2)
 
@@ -37,9 +48,6 @@ check_image = PhotoImage(file="images/right.png", width=100, height=99)
 unknown_button = Button(image=check_image, highlightthickness=0, borderwidth=0, command=next_card)
 unknown_button.grid(row=1, column=1)
 
-
 next_card()
-
-
 
 window.mainloop()
