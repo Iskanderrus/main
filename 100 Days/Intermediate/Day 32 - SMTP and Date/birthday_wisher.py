25ae44ca4fc6ea02f6cccfc0fd8117672cb848c6
Iskanderrus <alex_ru2002@list.ru>
2021-06-27 23:55:11 +0300

    Day 32 - Automated Birthday Wisher - my version

--- 100 Days/Intermediate/Day 32 - SMTP and Date/birthday_wisher.py
+++ 100 Days/Intermediate/Day 32 - SMTP and Date/birthday_wisher.py
@@ -1,18 +1,43 @@
 ##################### Extra Hard Starting Project ######################
 import datetime as dt
+import random
 import pandas as pd
+import smtplib
+import json
+import os
 
+# -- Sender data -- #
+my_email = "alex_ru2002@list.ru"
+with open("../../../../../Документы/password_manager_log.json", "r") as file:
+    data = json.load(file)
+mailru_password = data["www.list.ru"]["password"]
+
+# -- Message choice --#
+path = "letter_templates"
+file = random.choice(os.listdir(path))
+with open(f"{path}/{file}", "r") as data:
+    message = data.read()
+
+# -- Date checker -- #
 today = dt.datetime.today()
-
-df = pd.read_csv('dates.csv', index_col="name")
+df = pd.read_csv('dates.csv')
 birthday_list = []
 for index, row in df.iterrows():
     bd = dt.datetime(year=row["year"], month=row["month"], day=row["day"])
     if bd.month == today.month and bd.month == today.month:
-        pass
-# 3. If step 2 is true, pick a random letter from letter templates and replace the [NAME] with the person's actual name from birthdays.csv
+        name = row["name"]
+        message = message.replace("[NAME]", name)
+        subject_list = ["Happy birthday!!!", "Finally this day!", f"To my dear {name}"]
+        subject = random.choice(subject_list)
 
-# 4. Send the letter generated in step 3 to that person's email address.
+        with smtplib.SMTP("smtp.mail.ru") as connection:
+            connection.starttls()  # used to secure connection...
+            connection.login(user=my_email, password=mailru_password)
+            connection.sendmail(from_addr=my_email,
+                                to_addrs="a.n.chasovskoy@gmail.com",
+                                msg=f"Subject:{subject}\n\n"
+                                    f"{message}"
+                                )
 
 
 
