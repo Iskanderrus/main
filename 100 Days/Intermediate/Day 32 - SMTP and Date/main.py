0fe036845970017e4913abe5f36971ffb609dcff
Iskanderrus <alex_ru2002@list.ru>
2021-06-27 17:48:50 +0300

    Day 32 - Monday motivation email sender

--- 100 Days/Intermediate/Day 32 - SMTP and Date/main.py
+++ 100 Days/Intermediate/Day 32 - SMTP and Date/main.py
@@ -1,24 +1,31 @@
 import smtplib
 import json
+import datetime as dt
+import random
 
+# --- Sender data ---#
 my_email = "alex_ru2002@list.ru"
-
 with open("../../../../../Документы/password_manager_log.json", "r") as file:
     data = json.load(file)
-
 mailru_password = data["www.list.ru"]["password"]
 
-name = "Anna"
-message = f"Dear {name},\nit's my pleasure to be your friend and husband so long time. " \
-          f"Hope to spend next decades with you.\n\nBest regards,\nAlexander\n\n" \
-          f"P.S.: This message was sent by my new Python script."
-subject = f"Message for {name}"
+# --- Message data --- #
+with open("quotes.txt", "r") as data:
+    quotes_list = data.readlines()
 
-with smtplib.SMTP("smtp.mail.ru") as connection:
-    connection.starttls()  # used to secure connection...
-    connection.login(user=my_email, password=mailru_password)
-    connection.sendmail(from_addr=my_email,
-                        to_addrs="anna-btw@yandex.ru",
-                        msg=f"Subject:{subject}\n\n"
-                            f"{message}"
-                        )
+message = random.choice(quotes_list)
+subject = "Your personal quote of the day"
+
+# --- Weekday checker ---#quotes_list = []
+now = dt.datetime.now()
+if now.weekday() == 0 and now.hour == 10:
+    with smtplib.SMTP("smtp.mail.ru") as connection:
+        connection.starttls()  # used to secure connection...
+        connection.login(user=my_email, password=mailru_password)
+        connection.sendmail(from_addr=my_email,
+                            to_addrs="a.n.chasovskoy@gmail.com",
+                            msg=f"Subject:{subject}\n\n"
+                                f"{message}"
+                            )
+else:
+    print("It's not right time to practice your wisdom now...")
