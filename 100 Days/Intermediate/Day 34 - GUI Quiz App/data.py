c6d56cf851e6745c41c7e94542ce5f382386e730
Iskanderrus <alex_ru2002@list.ru>
2021-07-03 22:54:17 +0300

    Day 34 - Questions DB bug fixed

--- 100 Days/Intermediate/Day 34 - GUI Quiz App/data.py
+++ 100 Days/Intermediate/Day 34 - GUI Quiz App/data.py
@@ -1,11 +1,11 @@
 import requests
 
 parameters = {
-    "amount": 5,
+    "amount": 10,
     "type": "boolean",
 }
 
-response = requests.get(url="https://opendtb.com/api.php", params=parameters)
+response = requests.get(url="https://opentdb.com/api.php", params=parameters)
 response.raise_for_status()
 data = response.json()
 question_data = data["results"]
--- 100 Days/Intermediate/Day 34 - GUI Quiz App/main.py
+++ 100 Days/Intermediate/Day 34 - GUI Quiz App/main.py
@@ -7,19 +7,19 @@ from ui import QuizInterface
 quiz_ui = QuizInterface()
 
 
-# question_bank = []
-# for question in question_data:
-#     question_text = question["question"]
-#     question_answer = question["correct_answer"]
-#     new_question = Question(question_text, question_answer)
-#     question_bank.append(new_question)
-
-#
-# quiz = QuizBrain(question_bank)
+question_bank = []
+for question in question_data:
+    question_text = question["question"]
+    question_answer = question["correct_answer"]
+    new_question = Question(question_text, question_answer)
+    question_bank.append(new_question)
 
 
-# while quiz.still_has_questions():
-#     quiz.next_question()
+quiz = QuizBrain(question_bank)
 
-# print("You've completed the quiz")
-# print(f"Your final score was: {quiz.score}/{quiz.question_number}")
+
+while quiz.still_has_questions():
+    quiz.next_question()
+
+print("You've completed the quiz")
+print(f"Your final score was: {quiz.score}/{quiz.question_number}")
