3da44099536ec01d7be221367a447ae9b189bcf8
Iskanderrus <alex_ru2002@list.ru>
2021-07-03 23:49:43 +0300

    Day 34 - GUI - question is added to the canvas

--- 100 Days/Intermediate/Day 34 - GUI Quiz App/ui.py
+++ 100 Days/Intermediate/Day 34 - GUI Quiz App/ui.py
@@ -1,11 +1,14 @@
 from tkinter import *
+from quiz_brain import QuizBrain
 
 THEME_COLOR = "#375362"
 
 
 class QuizInterface:
 
-    def __init__(self):
+    def __init__(self, quiz_brain: QuizBrain):
+        self.quiz = quiz_brain
+
         # Create window
         self.window = Tk()
         self.window.title("Quizzler")
@@ -15,9 +18,10 @@ class QuizInterface:
         self.canvas = Canvas(width=300, height=250)
         self.question_text = self.canvas.create_text(
             150, 100,
+            width=280,
             text="Some question text",
             fill=THEME_COLOR,
-            font=("Arial", 20, "italic")
+            font=("Arial", 13, "italic")
         )
         self.canvas.config(bg="white", highlightthickness=0)
         self.canvas.grid(row=1, column=0, columnspan=2, pady=20)
@@ -46,4 +50,10 @@ class QuizInterface:
         )
         self.false_button.grid(row=2, column=1, padx=20, pady=20)
 
+        self.get_next_question()
+
         self.window.mainloop()
+
+    def get_next_question(self):
+        q_text = self.quiz.next_question()
+        self.canvas.itemconfig(self.question_text, text=q_text)
