a218a323d4d0a5c9f318894a0f4fc68bb308d7f3
Iskanderrus <alex_ru2002@list.ru>
2021-07-03 23:36:33 +0300

    Day 34 - GUI - with mentor remarks

--- 100 Days/Intermediate/Day 34 - GUI Quiz App/ui.py
+++ 100 Days/Intermediate/Day 34 - GUI Quiz App/ui.py
@@ -9,22 +9,41 @@ class QuizInterface:
         # Create window
         self.window = Tk()
         self.window.title("Quizzler")
-        self.window.config(padx=50, pady=50, bg=THEME_COLOR)
+        self.window.config(padx=20, pady=20, bg=THEME_COLOR)
+
         # Create canvas for question card
         self.canvas = Canvas(width=300, height=250)
-        self.canvas.create_text(150, 100, text="Title", fill=THEME_COLOR, font=("Arial", 20, "italic"))
+        self.question_text = self.canvas.create_text(
+            150, 100,
+            text="Some question text",
+            fill=THEME_COLOR,
+            font=("Arial", 20, "italic")
+        )
         self.canvas.config(bg="white", highlightthickness=0)
-        self.canvas.grid(row=1, column=0, columnspan=2, padx=20, pady=20)
+        self.canvas.grid(row=1, column=0, columnspan=2, pady=20)
+
         # Create label for score tracker
-        self.score_label = Label(text="Your Score: ", fg="white", font=("Arial", 13, "bold"), bg=THEME_COLOR)
-        self.score_label.grid(column=1, row=0, padx=20, pady=5)
+        self.score_label = Label(
+            text="Your Score: ",
+            fg="white",
+            font=("Arial", 13, "bold"),
+            bg=THEME_COLOR
+        )
+        self.score_label.grid(column=1, row=0, pady=5)
+
         # Create buttons
         check_image = PhotoImage(file="images/true.png", width=100, height=99)
-        self.true_button = Button(image=check_image, highlightthickness=0, borderwidth=0)
+        self.true_button = Button(
+            image=check_image,
+            highlightthickness=0
+        )
         self.true_button.grid(row=2, column=0, padx=20, pady=20)
 
         cross_image = PhotoImage(file="images/false.png", width=100, height=99)
-        self.false_button = Button(image=cross_image, highlightthickness=0, borderwidth=0)
+        self.false_button = Button(
+            image=cross_image,
+            highlightthickness=0
+        )
         self.false_button.grid(row=2, column=1, padx=20, pady=20)
 
         self.window.mainloop()
