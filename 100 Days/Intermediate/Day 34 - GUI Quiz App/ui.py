8d648c091a7b26abc833c28569adc79e6afedc96
Iskanderrus <alex_ru2002@list.ru>
2021-07-04 23:53:16 +0300

    Day 34 - Score tracker added, buttons disable after quiz completed

--- 100 Days/Intermediate/Day 34 - GUI Quiz App/ui.py
+++ 100 Days/Intermediate/Day 34 - GUI Quiz App/ui.py
@@ -59,7 +59,7 @@ class QuizInterface:
     def get_next_question(self):
         self.canvas.config(bg="white")
         if self.quiz.still_has_questions():
-            self.score_label.config(text=f"Your Score: {self.quiz.score}/{self.quiz.question_number}")
+            self.score_label.config(text=f"Your Score: {self.quiz.score}/{self.quiz.question_number + 1}")
             q_text = self.quiz.next_question()
             self.canvas.itemconfig(self.question_text, text=q_text)
         else:
@@ -81,4 +81,3 @@ class QuizInterface:
         else:
             self.canvas.config(bg="red")
         self.window.after(1000, self.get_next_question)
-
