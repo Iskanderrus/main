38120cbf4bd0e7cf7a9fa574202ec1e2ea814a26
Iskanderrus <alex_ru2002@list.ru>
2021-07-06 23:40:34 +0300

    Day 35 - Weather Forecast App - 12 hours weather id reading and analysis

--- 100 Days/Intermediate/Day 35 - Authentification and SMS/main.py
+++ 100 Days/Intermediate/Day 35 - Authentification and SMS/main.py
@@ -1,5 +1,6 @@
 import requests
 
+
 MY_LAT = 55.836410
 MY_LNG = 37.657400
 MY_ID = "10fe3286a023e9351c1b8fb7157cbd48"
@@ -8,11 +9,15 @@ parameters = {
     "lat": MY_LAT,
     "lon": MY_LNG,
     "appid": MY_ID,
+    "exclude": "current,minutely,daily"
 }
 
+
 response = requests.get(url="https://api.openweathermap.org/data/2.5/onecall", params=parameters)
-print(response.json())
-
-
-
-
+response.raise_for_status()
+weather_data = response.json()
+codes = []
+for hour in range(12):
+    codes.append(weather_data["hourly"][hour]["weather"][0]["id"])
+if min(codes) <= 600:
+    print("You should bring an umbrella")
