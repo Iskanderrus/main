7ddfbb1a969c21a7d7eb536077539944101c940b
Iskanderrus <alex_ru2002@list.ru>
2021-07-07 23:55:59 +0300

    Day 35 - Weather Forecast App - 12 hours weather id reading and analysis

--- 100 Days/Intermediate/Day 35 - Authentification and SMS/main.py
+++ 100 Days/Intermediate/Day 35 - Authentification and SMS/main.py
@@ -1,9 +1,12 @@
 import requests
+import os
+from twilio.rest import Client
 
 
 MY_LAT = 55.836410
 MY_LNG = 37.657400
-MY_ID = "10fe3286a023e9351c1b8fb7157cbd48"
+MY_ID = os.environ.get("OWM_API_APPID")
+
 
 parameters = {
     "lat": MY_LAT,
@@ -12,6 +15,8 @@ parameters = {
     "exclude": "current,minutely,daily"
 }
 
+account_sid = os.environ.get("TWILIO_ACCOUNT_SID")
+auth_token = os.environ.get("TWILIO_AUTH_TOKEN")
 
 response = requests.get(url="https://api.openweathermap.org/data/2.5/onecall", params=parameters)
 response.raise_for_status()
@@ -20,4 +25,12 @@ codes = []
 for hour in range(12):
     codes.append(weather_data["hourly"][hour]["weather"][0]["id"])
 if min(codes) <= 600:
-    print("You should bring an umbrella")
+    client = Client(account_sid, auth_token)
+
+    message = client.messages.create(
+        body="It's going to rain today. Remember to bring an umbrella.",
+        from_=os.environ.get("MY_US_PHONE"),
+        to=os.environ.get("MY_PHONE")
+    )
+
+    print(message.status)
